9db9a0e09d78698f76e31378119024fe1b431cbb
bfren <bf@bfren.dev>
2023-03-03 17:16:44 +0000

    Adding create user query

---
+++ src/Domain/CreateUser/CreateUserQuery.cs
@@ -0,0 +1,17 @@
+// Clinical Skills
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+using Jeebs.Auth.Data;
+using Jeebs.Cqrs;
+
+namespace ClinicalSkills.Domain.CreateUser;
+
+/// <inheritdoc cref="CreateUserHandler"/>
+/// <param name="Name">User's name</param>
+/// <param name="EmailAddress">Email address (for login)</param>
+/// <param name="Password">Password (will be hashed)</param>
+public sealed record class CreateUserQuery(
+	string Name,
+	string EmailAddress,
+	string Password
+) : Query<AuthUserId>;
