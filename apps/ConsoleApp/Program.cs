694592306370b7669d13cd422b4fecd010645078
bfren <bf@bfren.dev>
2023-03-03 11:29:28 +0000

    Adding console app for testing

---
+++ apps/ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿// Clinical Skills Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+using ClinicalSkills.Persistence.Clients.Sqlite;
+using Jeebs.Cqrs;
+using Microsoft.Extensions.DependencyInjection;
+using D = ClinicalSkills.Domain;
+
+// ==========================================
+//  CONFIGURE
+// ==========================================
+
+var (app, log) = Jeebs.Apps.Host.Create(args, (_, svc) => svc.AddData().AddCqrs());
+
+// ==========================================
+//  BEGIN
+// ==========================================
+
+log.Inf("Clinical Skills Console app.");
+
+// ==========================================
+//  SETUP
+// ==========================================
+
+var dispatcher = app.Services.GetRequiredService<IDispatcher>();
+
+static void Write(string text)
+{
+	var pad = new string('=', text.Length + 6);
+	Console.WriteLine();
+	Console.WriteLine(pad);
+	Console.WriteLine($"== {text} ==");
+	Console.WriteLine(pad);
+}
+
+static void Pause()
+{
+	Write("PAUSE");
+	Console.WriteLine("Press any key when ready.");
+	_ = Console.ReadLine();
+}
+
+// ==========================================
+//  RUN MIGRATIONS
+// ==========================================
+
+Write("Migrations");
+log.Inf("Migrate to latest database version.");
+await dispatcher.DispatchAsync(
+	new D.MigrateToLatest.MigrateToLatestCommand()
+);
+
+// ==========================================
+//  DONE
+// ==========================================
+
+Write("Done");
