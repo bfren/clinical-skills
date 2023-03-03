bbd1359d7f30ed7ae5f0de0101f4e36ce439ba8c
bfren <bf@bfren.dev>
2023-03-03 17:16:58 +0000

    Only show sidebar if user is signed in

--- apps/WebApp/Pages/Components/Sidebar/Default.cshtml.cs
+++ apps/WebApp/Pages/Components/Sidebar/Default.cshtml.cs
@@ -6,30 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace ClinicalSkills.WebApp.Pages.Components.Sidebar;
 
 public sealed record class SidebarModel(
-	Menu[] Menus
+	Menu[]? Menus
 );
 
 public class SidebarViewComponent : ViewComponent
 {
 	public IViewComponentResult Invoke()
 	{
-		var mainMenu = new Menu(
-			Title: "Menu",
-			Items: new MenuItem[]
-			{
-				new("Entries", "/Entries/Index")
-			}
-		);
+		if (HttpContext.User.Identity?.IsAuthenticated == true)
+		{
+			var mainMenu = new Menu(
+				Title: "Menu",
+				Items: new MenuItem[]
+				{
+					new("Entries", "/Entries/Index")
+				}
+			);
 
-		var accountMenu = new Menu(
-			Title: "Account",
-			Items: new MenuItem[]
-			{
-				new("Profile", "/Auth/Profile"),
-				new("Sign Out", "/Auth/Signout")
-			}
-		);
+			var accountMenu = new Menu(
+				Title: "Account",
+				Items: new MenuItem[]
+				{
+					new("Profile", "/Auth/Profile"),
+					new("Sign Out", "/Auth/SignOut")
+				}
+			);
 
-		return View(new SidebarModel(new[] { mainMenu, accountMenu }));
+			return View(new SidebarModel(new[] { mainMenu, accountMenu }));
+		}
+
+		return View(new SidebarModel(null));
 	}
 }
