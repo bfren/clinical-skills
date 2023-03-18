9bba9a2546ac062138d5ecc82862abf289f76c3d
bfren <bf@bfren.dev>
2023-03-18 17:01:30 +0000

    Improving abstract TestHandler

--- tests/Tests.Domain/- Abstracts -/TestHandler.cs
+++ tests/Tests.Domain/- Abstracts -/TestHandler.cs
@@ -4,100 +4,76 @@
 using Jeebs.Cqrs;
 using Jeebs.Data;
 using Jeebs.Data.Query;
+using Jeebs.Data.Testing.Query;
 using Jeebs.Logging;
 using MaybeF.Caching;
-using NSubstitute.Extensions;
 using StrongId;
 
 namespace Abstracts;
 
-public abstract class TestHandler
+internal abstract class TestHandlerBase<THandler>
 {
-	internal record class Vars<THandler>()
+	internal record class Vars
 	{
-		public IDispatcher Dispatcher { get; init; } =
-			Substitute.For<IDispatcher>();
+		internal IDispatcher Dispatcher { get; init; }
 
-		public ILog<THandler> Log { get; init; } =
-			Substitute.For<ILog<THandler>>();
-	}
-
-	internal record class Vars<TRepo, TEntity, TId, THandler> : Vars<THandler>
-		where TRepo : class, IRepository<TEntity, TId>
-		where TEntity : IWithId<TId>
-		where TId : class, IStrongId, new()
-	{
-		public IMaybeCache<TId> Cache { get; init; } =
-			Substitute.For<IMaybeCache<TId>>();
-
-		public IFluentQuery<TEntity, TId> Fluent { get; init; } =
-			Substitute.For<IFluentQuery<TEntity, TId>>();
-
-		public TRepo Repo { get; init; } =
-			Substitute.For<TRepo>();
+		internal ILog<THandler> Log { get; init; }
 
 		public Vars()
 		{
-			Fluent.ReturnsForAll(Fluent);
-			Repo.StartFluentQuery().ReturnsForAll(Fluent);
+			// Create substitutes
+			Dispatcher = Substitute.For<IDispatcher>();
+			Log = Substitute.For<ILog<THandler>>();
 		}
 	}
 
-	internal abstract class Setup<THandler> : Setup<THandler, Vars<THandler>> { }
+	internal abstract class SetupBase : SetupBase<Vars> { }
 
-	internal abstract class Setup<THandler, TVars>
-		where TVars : Vars<THandler>, new()
+	internal abstract class SetupBase<TVars>
+		where TVars : Vars, new()
 	{
 		internal abstract THandler GetHandler(TVars v);
 
 		internal virtual (THandler handler, TVars v) GetVars()
 		{
-			// Create substitutes
-			var dispatcher = Substitute.For<IDispatcher>();
-			var log = Substitute.For<ILog<THandler>>();
-
 			// Build handler
-			var v = new TVars { Dispatcher = dispatcher, Log = log };
+			var v = new TVars();
 			var handler = GetHandler(v);
 
 			// Return vars
 			return (handler, v);
 		}
 	}
+}
 
-	internal abstract class Setup<TRepo, TEntity, TId, THandler> : Setup<TRepo, TEntity, TId, THandler, Vars<TRepo, TEntity, TId, THandler>>
-		where TRepo : class, IRepository<TEntity, TId>
-		where TEntity : IWithId<TId>
-		where TId : class, IStrongId, new()
-	{ }
-
-	internal abstract class Setup<TRepo, TEntity, TId, THandler, TVars>
-		where TRepo : class, IRepository<TEntity, TId>
-		where TEntity : IWithId<TId>
-		where TId : class, IStrongId, new()
-		where TVars : Vars<TRepo, TEntity, TId, THandler>, new()
+internal abstract class TestHandlerBase<TRepo, TEntity, TId, THandler> : TestHandlerBase<THandler>
+	where TRepo : class, IRepository<TEntity, TId>
+	where TEntity : IWithId<TId>
+	where TId : class, IStrongId, new()
+{
+	internal new record class Vars : TestHandlerBase<THandler>.Vars
 	{
-		internal abstract THandler GetHandler(TVars v);
+		internal IMaybeCache<TId> Cache { get; init; }
 
-		internal virtual (THandler handler, TVars v) GetVars()
+		internal IFluentQuery<TEntity, TId> Fluent { get; init; }
+
+		internal TRepo Repo { get; init; }
+
+		public Vars()
 		{
 			// Create substitutes
-			var cache = Substitute.For<IMaybeCache<TId>>();
-			var dispatcher = Substitute.For<IDispatcher>();
-			var fluent = Substitute.For<IFluentQuery<TEntity, TId>>();
-			var log = Substitute.For<ILog<THandler>>();
-			var repo = Substitute.For<TRepo>();
+			Cache = Substitute.For<IMaybeCache<TId>>();
+			Fluent = FluentQueryHelper.CreateSubstitute<TEntity, TId>();
+			Repo = Substitute.For<TRepo>();
 
 			// Setup substitutes
-			fluent.ReturnsForAll(fluent);
-			repo.StartFluentQuery().Returns(fluent);
-
-			// Build handler
-			var v = new TVars { Cache = cache, Dispatcher = dispatcher, Fluent = fluent, Log = log, Repo = repo };
-			var handler = GetHandler(v);
-
-			// Return vars
-			return (handler, v);
+			Repo.StartFluentQuery().Returns(Fluent);
 		}
 	}
+
+	internal new abstract class SetupBase : SetupBase<Vars> { }
+
+	internal new abstract class SetupBase<TVars> : TestHandlerBase<THandler>.SetupBase<TVars>
+		where TVars : Vars, new()
+	{ }
 }
