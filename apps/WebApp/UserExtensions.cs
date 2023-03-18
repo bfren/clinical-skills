ccb08f350f277dd72b10978fc6c5b0b0783b77e6
bfren <bf@bfren.dev>
2023-03-18 16:00:41 +0000

    Adding GetEncryptionKey() extension method for User object

---
+++ apps/WebApp/UserExtensions.cs
@@ -0,0 +1,19 @@
+// Clinical Skills Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+using System.Security.Claims;
+using MaybeF.Extensions;
+
+namespace ClinicalSkills.WebApp;
+
+public static class UserExtensions
+{
+	public static Maybe<string> GetEncryptionKey(this ClaimsPrincipal principal) =>
+		principal.Claims
+			.SingleOrNone(
+				c => c.Type == Domain.ClaimTypes.EncryptionKey
+			)
+			.Bind(
+				c => c.Value.Some()
+			);
+}
