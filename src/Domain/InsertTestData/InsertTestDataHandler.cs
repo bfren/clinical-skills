d3fec3e8f098731d91a86f109e93c1706f3e71dd
bfren <bf@bfren.dev>
2023-03-22 15:38:17 +0000

    Inserting test training grades and entries

--- src/Domain/InsertTestData/InsertTestDataHandler.cs
+++ src/Domain/InsertTestData/InsertTestDataHandler.cs
@@ -1,11 +1,17 @@
 // Clinical Skills
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using ClinicalSkills.Domain.CreateUser;
 using ClinicalSkills.Domain.SaveClinicalSetting.Internals;
+using ClinicalSkills.Domain.SaveEntry.Internals;
+using ClinicalSkills.Domain.SaveTrainingGrade.Internals;
+using ClinicalSkills.Persistence.StrongIds;
+using Jeebs.Auth.Data;
 using Jeebs.Cqrs;
+using Jeebs.Cryptography.Functions;
 using Jeebs.Logging;
+using RndF;
 
 namespace ClinicalSkills.Domain.InsertTestData;
 
@@ -35,12 +41,42 @@ internal sealed class InsertTestDataHandler : CommandHandler<InsertTestDataComma
 		Log.Inf("Inserting test data.");
 		var pwd = "fred";
 		var q = from u0 in Dispatcher.DispatchAsync(new CreateUserQuery("bf", "info@bfren.dev", pwd))
+				from ec0 in Dispatcher.DispatchAsync(new GetUserEncryptionKeyQuery(u0, pwd))
 				from cs0 in Dispatcher.DispatchAsync(new CreateClinicalSettingQuery(u0, "ED"))
 				from cs1 in Dispatcher.DispatchAsync(new CreateClinicalSettingQuery(u0, "MAU"))
+				from tg0 in Dispatcher.DispatchAsync(new CreateTrainingGradeQuery(u0, Rnd.Str))
+				from tg1 in Dispatcher.DispatchAsync(new CreateTrainingGradeQuery(u0, Rnd.Str))
+				from _ in insertEntries(u0, cs0, cs1, tg0, tg1, ec0)
 				select true;
 
 		return await q.AuditAsync(
 			none: r => Log.Msg(r, LogLevel.Error)
 		);
+
+		async Task<Maybe<IEnumerable<EntryId>>> insertEntries(
+			AuthUserId userId,
+			ClinicalSettingId clinicalSettingId0, ClinicalSettingId clinicalSettingId1,
+			TrainingGradeId trainingGradeId0, TrainingGradeId trainingGradeId1,
+			string encryptionKey
+		)
+		{
+			Log.Inf("Inserting test entries.");
+
+			var entryIds = new List<EntryId>();
+			for (var i = 0; i < 10; i++)
+			{
+				var clinicalSettingId = Rnd.Flip ? clinicalSettingId0 : clinicalSettingId1;
+				var trainingGradeId = Rnd.Flip ? trainingGradeId0 : trainingGradeId1;
+				var patientAge = Rnd.NumberF.GetInt32(100);
+				var caseSummary = CryptoF.Lock(Rnd.Str, encryptionKey);
+				var learningPoints = CryptoF.Lock(Rnd.Str, encryptionKey);
+
+				_ = await Dispatcher
+					.DispatchAsync(new CreateEntryQuery(userId, Rnd.DateTime, clinicalSettingId, trainingGradeId, patientAge, caseSummary, learningPoints))
+					.IfSomeAsync(entryIds.Add);
+			}
+
+			return entryIds;
+		}
 	}
 }
