8f211d131689ca01f693fb6a90139f6e79e83d9e
bfren <bf@bfren.dev>
2023-03-23 10:29:57 +0000

    Adding GetSingle abstract test class

---
+++ tests/Tests.Domain/- Abstracts -/GetSingle/HandleAsync_Tests.cs
@@ -0,0 +1,165 @@
+// Clinical Skills: Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+using Domain;
+using Jeebs.Auth.Data;
+using Jeebs.Cqrs;
+using Jeebs.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Testing.Query;
+using Jeebs.Messages;
+using StrongId;
+
+namespace Abstracts.GetSingle;
+
+public abstract class HandleAsync_Tests
+{
+	public abstract Task Test00_Id_Is_Null__Returns_None_With_NullMsg();
+
+	public abstract Task Test01_Calls_Log_Vrb__With_Correct_Values();
+
+	public abstract Task Test02_Calls_FluentQuery_Where__With_Correct_Values();
+
+	public abstract Task Test03_Calls_FluentQuery_QuerySingleAsync();
+
+	public abstract Task Test04_Calls_FluentQuery_QuerySingleAsync__Different_UserId__Returns_None_With_DoesNotBelongToUserMsg();
+
+	public abstract Task Test05_Calls_FluentQuery_QuerySingleAsync__Same_UserId__Returns_Result();
+
+	internal abstract class TestHandlerBase<TRepo, TEntity, TId, TQuery, THandler, TModel> : TestHandlerBase<TRepo, TEntity, TId, THandler>
+		where TRepo : class, IRepository<TEntity, TId>
+		where TEntity : IWithId<TId>
+		where TId : LongId, IStrongId, new()
+		where TQuery : Query<TModel>
+		where THandler : QueryHandler<TQuery, TModel>
+		where TModel : IWithUserId, IWithId<TId>
+	{
+		internal new abstract class SetupBase : SetupBase<Vars>
+		{
+			protected SetupBase(bool enableCache, string name) : base(enableCache, name) { }
+		}
+
+		internal new abstract class SetupBase<TVars> : TestHandlerBase<TRepo, TEntity, TId, THandler>.SetupBase<TVars>
+			where TVars : Vars, new()
+		{
+			internal bool EnableCache { get; }
+
+			internal string Name { get; }
+
+			internal abstract TQuery GetQuery(AuthUserId? userId = null, TId? entityId = null);
+
+			internal abstract TModel NewModel { get; }
+
+			protected SetupBase(bool enableCache, string name) =>
+				(EnableCache, Name) = (enableCache, name);
+
+			internal override (THandler handler, TVars v) GetVars()
+			{
+				var (handler, v) = base.GetVars();
+				if (EnableCache)
+				{
+					v.Cache.GetOrCreateAsync(Arg.Any<TId>(), Arg.Any<Func<Task<Maybe<TModel>>>>())
+						.Returns(x => x.ArgAt<Func<Task<Maybe<TModel>>>>(1).Invoke());
+
+					v.Fluent.QuerySingleAsync<TModel>()
+						.Returns(NewModel);
+				}
+
+				return (handler, v);
+			}
+
+			internal async Task Test00<TIdIsNullMsg>(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+				where TIdIsNullMsg : IMsg
+			{
+				// Arrange
+				var (handler, _) = GetVars();
+				var query = GetQuery(LongId<AuthUserId>(), new() { Value = 0 });
+
+				// Act
+				var result = await handle(handler, query);
+
+				// Assert
+				result.AssertNone().AssertType<TIdIsNullMsg>();
+			}
+
+			internal async Task Test01(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+			{
+				// Arrange
+				var (handler, v) = GetVars();
+				var query = GetQuery();
+
+				// Act
+				_ = await handle(handler, query);
+
+				// Assert
+				v.Log.Received().Vrb($"Get {Name}: {{Query}}.", query);
+			}
+
+			internal async Task Test02(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+			{
+				// Arrange
+				var (handler, v) = GetVars();
+				var userId = LongId<AuthUserId>();
+				var entityId = LongId<TId>();
+				var query = GetQuery(userId, entityId);
+
+				// Act
+				_ = await handle(handler, query);
+
+				// Assert
+				v.Fluent.AssertCalls(
+					c => FluentQueryHelper.AssertWhere<TEntity, TId>(c, x => x.Id, Compare.Equal, entityId),
+					c => FluentQueryHelper.AssertWhere<TEntity, AuthUserId>(c, "UserId", Compare.Equal, userId),
+					_ => { }
+				);
+			}
+
+			internal async Task Test03(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+			{
+				// Arrange
+				var (handler, v) = GetVars();
+				var query = GetQuery();
+
+				// Act
+				_ = await handle(handler, query);
+
+				// Assert
+				await v.Fluent.Received().QuerySingleAsync<TModel>();
+			}
+
+			internal async Task Test04<TDoesNotBelongToUserMsg>(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+				where TDoesNotBelongToUserMsg : Msg
+			{
+				// Arrange
+				var (handler, v) = GetVars();
+				var model = NewModel;
+				v.Fluent.QuerySingleAsync<TModel>()
+					.Returns(model);
+				var query = GetQuery();
+
+				// Act
+				var result = await handle(handler, query);
+
+				// Assert
+				result.AssertNone().AssertType<TDoesNotBelongToUserMsg>();
+			}
+
+			internal async Task Test05(Func<THandler, TQuery, Task<Maybe<TModel>>> handle)
+			{
+				// Arrange
+				var (handler, v) = GetVars();
+				var model = NewModel;
+				v.Fluent.QuerySingleAsync<TModel>()
+					.Returns(model);
+				var query = GetQuery(model.UserId, model.Id);
+
+				// Act
+				var result = await handle(handler, query);
+
+				// Assert
+				var some = result.AssertSome();
+				Assert.Equal(model, some);
+			}
+		}
+	}
+}
