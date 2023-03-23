ee75b341ae82be1b8e941a4810f487491cb8e5a2
bfren <bf@bfren.dev>
2023-03-23 10:30:14 +0000

    Adding additional options to GetEntries

--- src/Domain/GetEntries/GetEntriesHandler.cs
+++ src/Domain/GetEntries/GetEntriesHandler.cs
@@ -34,15 +34,38 @@ internal sealed class GetEntriesHandler : QueryHandler<GetEntriesQuery, IEnumera
 	/// <param name="query"></param>
 	public override Task<Maybe<IEnumerable<EntryModel>>> HandleAsync(GetEntriesQuery query)
 	{
-		var start = DateTime.SpecifyKind(query.Start, DateTimeKind.Unspecified);
-		var end = DateTime.SpecifyKind(query.End, DateTimeKind.Unspecified);
+		Log.Vrb("Getting Entries for {User} matching {Query}.", query.UserId.Value, query);
 
-		Log.Vrb("Getting Entries for {User} between {Start} and {End}.", query.UserId.Value, start, end);
-		return Entry
-			.StartFluentQuery()
-			.Where(x => x.UserId, Compare.Equal, query.UserId)
-			.Where(x => x.DateOccurred, Compare.MoreThanOrEqual, start)
-			.Where(x => x.DateOccurred, Compare.LessThanOrEqual, end)
+		// Start query
+		var fluent = Entry.StartFluentQuery()
+			.Where(x => x.UserId, Compare.Equal, query.UserId);
+
+		// Add dates from / to
+		if (query.DateOccurredFrom is DateTime dateOccurredFrom)
+		{
+			dateOccurredFrom = DateTime.SpecifyKind(dateOccurredFrom, DateTimeKind.Unspecified);
+			fluent = fluent.Where(x => x.DateOccurred, Compare.MoreThanOrEqual, dateOccurredFrom);
+		}
+
+		if (query.DateOccurredTo is DateTime dateOccurredTo)
+		{
+			dateOccurredTo = DateTime.SpecifyKind(dateOccurredTo, DateTimeKind.Unspecified);
+			fluent = fluent.Where(x => x.DateOccurred, Compare.LessThanOrEqual, dateOccurredTo);
+		}
+
+		// Patient age from / to
+		if (query.PatientAgeFrom is int patientAgeFrom)
+		{
+			fluent = fluent.Where(x => x.PatientAge, Compare.MoreThanOrEqual, patientAgeFrom);
+		}
+
+		if (query.PatientAgeTo is int patientAgeTo)
+		{
+			fluent = fluent.Where(x => x.PatientAge, Compare.LessThanOrEqual, patientAgeTo);
+		}
+
+		// Add sort and execute query
+		return fluent
 			.Sort(x => x.DateOccurred, SortOrder.Descending)
 			.QueryAsync<EntryModel>();
 	}
--- src/Domain/_/GetEntriesQuery.cs
+++ src/Domain/_/GetEntriesQuery.cs
@@ -6,15 +6,35 @@ using System.Collections.Generic;
 using Domain.GetEntries;
 using Jeebs.Auth.Data;
 using Jeebs.Cqrs;
+using Persistence.StrongIds;
 
 namespace Domain;
 
 /// <inheritdoc cref="GetEntriesHandler"/>
 /// <param name="UserId"></param>
-/// <param name="Start"></param>
-/// <param name="End"></param>
+/// <param name="DateOccurredFrom"></param>
+/// <param name="DateOccurredTo"></param>
+/// <param name="ClinicalSettingIds"></param>
+/// <param name="TrainingGradeIds"></param>
+/// <param name="PatientAgeFrom"></param>
+/// <param name="PatientAgeTo"></param>
+/// <param name="SkillIds"></param>
+/// <param name="ThemeIds"></param>
 public sealed record class GetEntriesQuery(
 	AuthUserId UserId,
-	DateTime Start,
-	DateTime End
-) : Query<IEnumerable<EntryModel>>;
+	DateTime? DateOccurredFrom,
+	DateTime? DateOccurredTo,
+	ClinicalSettingId[]? ClinicalSettingIds,
+	TrainingGradeId[]? TrainingGradeIds,
+	int? PatientAgeFrom,
+	int? PatientAgeTo,
+	SkillId[]? SkillIds,
+	ThemeId[]? ThemeIds
+) : Query<IEnumerable<EntryModel>>
+{
+	/// <summary>
+	/// Create basic query with only User ID
+	/// </summary>
+	/// <param name="userId"></param>
+	public GetEntriesQuery(AuthUserId userId) : this(userId, null, null, null, null, null, null, null, null) { }
+}
